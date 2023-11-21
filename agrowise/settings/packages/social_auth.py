5d23c6b8c534046f6a2a77b1977471f6659a289f
olaifa ibukun <ibukunolaifa1984@example.com>
2023-11-21 20:20:27 +0100

    updated social_auth settings

--- agrowise/settings/packages/social_auth.py
+++ agrowise/settings/packages/social_auth.py
@@ -1,7 +1,13 @@
 from agrowise.utils.env_variable import get_env_variable
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = get_env_variable("SOCIAL_AUTH_GOOGLE_OAUTH2_KEY")
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = get_env_variable("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET")
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = get_env_variable(
+    "SOCIAL_AUTH_GOOGLE_OAUTH2_KEY",
+    "XXX-XXX"
+)
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = get_env_variable(
+    "SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET",
+    "XXX-XXX"
+)
 SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
     "https://www.googleapis.com/auth/userinfo.email",
     "https://www.googleapis.com/auth/userinfo.profile",
