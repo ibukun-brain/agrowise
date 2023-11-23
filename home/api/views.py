4f5f301f917a1e09ac4b11983d23f63d688d935b
olaifa ibukun <ibukunolaifa1984@example.com>
2023-11-23 16:39:02 +0100

    added open api endpoint documentation

--- home/api/views.py
+++ home/api/views.py
@@ -17,7 +17,9 @@ class OpenAPIView(generics.CreateAPIView):
     )
     def post(self, request, *args, **kwargs):
         """
-        API endpoint for open api
+        API endpoint for open api answers response to question is related to crops
+        or agriculture or pest or pest alert or weather or weather forecast or crop
+        market insight or agricultural market insight
         """
         serializer = OpenAPISerializer(data=request.data)
         if serializer.is_valid():
