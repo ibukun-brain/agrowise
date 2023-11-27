16bd16259c43d159406f5d8afc3bdcfa710e05db
olaifa ibukun <ibukunolaifa1984@example.com>
2023-11-27 15:11:27 +0100

    added AI chat history

--- home/api/views.py
+++ home/api/views.py
@@ -8,7 +8,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from agrowise.utils.env_variable import get_env_variable
-from home.api.serializers import OpenAPISerializer, WeatherForecastSerializer
+from home.api.serializers import (
+    AIChatHistorySerializer,
+    OpenAPISerializer,
+    WeatherForecastSerializer,
+)
+from home.models import AIChatHistory
 
 # from home.tasks import weather_forecast_task
 
@@ -35,7 +40,7 @@ class OpenAPIView(generics.CreateAPIView):
                 "if the question is related to crops or agriculture or"
                 + "pest or pest alert or weather or weather forecast or crop market"
                 + "insight or agricultural market insight"
-                + f" - answer it: {prompt} else say ask question answer this!"
+                + f" - answer it: {prompt} else say can't answer this!"
             )
             chat_completion = client.chat.completions.create(
                 messages=[
@@ -44,16 +49,54 @@ class OpenAPIView(generics.CreateAPIView):
                         "content": prompt,
                     }
                 ],
-                model="gpt-4-1106-preview",
+                model="gpt-3.5-turbo",
                 max_tokens=256,
                 temperature=0.5,
             )
             response = chat_completion.choices[0].message.content
-            return Response(data={"data": response}, status=status.HTTP_200_OK)
+            resp = Response(data={"data": response}, status=status.HTTP_200_OK)
+            AIChatHistory.objects.create(
+                title=prompt,
+                user=request.user,
+                response=response,
+            )
+            return resp
         return Response(
             data={"data": "unable to process request"},
             status=status.HTTP_400_BAD_REQUEST,
         )
+
+
+class OpenAIHistoryListView(generics.ListAPIView):
+    serializer_class = AIChatHistorySerializer
+
+    def get(self, request, *args, **kwargs):
+        """
+        Endpoint to return Open AI chat history
+        """
+        return self.list(request, *args, **kwargs)
+
+    def get_queryset(self):
+        qs = AIChatHistory.objects.select_related("user") \
+            .filter(user=self.request.user)
+        return qs
+
+
+class OpenAIHistoryDetailView(generics.RetrieveAPIView):
+    serializer_class = AIChatHistorySerializer
+    lookup_url_kwarg = "uid"
+
+    def get(self, request, *args, **kwargs):
+        """
+        Endpoint to return a single Open AI chat history
+        """
+        return self.retrieve(request, *args, **kwargs)
+
+    def get_object(self):
+        uid = self.kwargs["uid"]
+        qs = AIChatHistory.objects.select_related("user") \
+            .get(uid=uid)
+        return qs
 
 
 class WeatherForecastAPIView(APIView):
